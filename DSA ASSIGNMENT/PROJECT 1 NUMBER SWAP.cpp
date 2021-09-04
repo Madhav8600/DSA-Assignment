44681596fbc09b64057a4514eeecb94500739563
Maddy <46467004+Madhav8600@users.noreply.github.com>
2021-09-04 20:45:34 +0530

    Add files via upload

---
+++ DSA ASSIGNMENT/PROJECT 1 NUMBER SWAP.cpp
@@ -0,0 +1,20 @@
+//Code is Written By Madhav
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 50, b = 100, temp;
+
+    cout << "Before swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    temp = a;
+    a = b;
+    b = temp;
+
+    cout << "\nAfter swapping." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
